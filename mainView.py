c092c4a8a8d513fa60a511b87c8f0e8a96ee03c0
Val <valeria.carvajal01@correo.usa.edu.co>
2021-04-26 13:19:34 -0500

    Correction Main frame

--- mainView.py
+++ mainView.py
@@ -31,14 +31,18 @@ class mainView:
         self.text = ''
         self.color = ["#f29844"]
         self.heading = Label(self.window,text=self.txt, font=("yu gothic ui", 30, "bold"),bg="white",fg="black",bd=5,relief=FLAT)
-        self.heading.place(x=95,y=180,width=440)
+        self.heading.place(x=125,y=185,width=300)
         self.heading_color()
 
         #Title 2
         self.txt2 = "Señal Moduladora"
         self.heading2 = Label(self.window,text=self.txt2, font=("yu gothic ui", 30, "bold"),bg="white",fg=self.color ,bd=5,relief=FLAT)
-        self.heading2.place(x=758,y=180,width=440)
-
+        self.heading2.place(x=825,y=185,width=350)
+        
+        #Title 3
+        self.txt3 = "Datos Generales"
+        self.heading3 = Label(self.window,text=self.txt3, font=("yu gothic ui", 20, "bold"),bg="white",fg=self.color ,bd=5,relief=FLAT)
+        self.heading3.place(x=537,y=185,width=198)
 
         #INPUTS
 
@@ -47,34 +51,52 @@ class mainView:
         self.ampc_label.place(x=150, y=260)
 
         self.ampc_entry = Entry(self.window, relief=FLAT,bg = "white",fg ="#6b6a69",font=("yu gothic ui semibold", 14))
-        self.ampc_entry.place(x=188,y=309,width=289)
+        self.ampc_entry.place(x=188,y=304,width=205)
 
         #Frequency c Label and Entry
         self.frec_label = Label(self.window, text="Frecuencia", bg="white", fg="#353534",font=("yu gothic ui", 18, "bold"))
-        self.frec_label.place(x=150, y=385)
+        self.frec_label.place(x=150, y=370)
 
         self.frec_entry = Entry(self.window, relief=FLAT, bg="white", fg="#6b6a69", font=("yu gothic ui semibold", 14))
-        self.frec_entry.place(x=188, y=426, width=289)
+        self.frec_entry.place(x=188, y=419, width=205)
 
         #Amplitude m Label and Entry
         self.ampm_label = Label(self.window, text="Amplitud", bg="white", fg="#353534",font=("yu gothic ui", 18, "bold"))
-        self.ampm_label.place(x=798, y=260)
+        self.ampm_label.place(x=870, y=260)
 
         self.ampm_entry = Entry(self.window, relief=FLAT, bg="white", fg="#6b6a69",font=("yu gothic ui semibold", 14))
-        self.ampm_entry.place(x=845, y=309, width=289)
+        self.ampm_entry.place(x=925, y=303, width=205)
 
         #Frequency m Label and Entry
         self.frem_label = Label(self.window, text="Frecuencia", bg="white", fg="#353534",font=("yu gothic ui", 18, "bold"))
-        self.frem_label.place(x=798, y=385)
+        self.frem_label.place(x=870, y=370)
 
         self.frem_entry = Entry(self.window, relief=FLAT, bg="white", fg="#6b6a69", font=("yu gothic ui semibold", 14))
-        self.frem_entry.place(x=845, y=425, width=289)
+        self.frem_entry.place(x=925, y=418, width=205)
 
+        #Modulation index Label and Entry
+        self.modi_label = Label(self.window, text="Índice de\n Modulación", bg="white", fg="#353534",font=("yu gothic ui", 17, "bold"))
+        self.modi_label.place(x=565, y=235)
+
+        self.modi_entry = Entry(self.window, relief=FLAT, bg="white", fg="#6b6a69",font=("yu gothic ui semibold", 14))
+        self.modi_entry.place(x=565, y=301, width=150)
+
+        #Load Resistance Label and Entry
+        self.lres_label = Label(self.window, text="Resistencia\n de Carga", bg="white", fg="#353534",font=("yu gothic ui", 17, "bold"))
+        self.lres_label.place(x=573, y=345)
+
+        self.lres_entry = Entry(self.window, relief=FLAT, bg="white", fg="#6b6a69", font=("yu gothic ui semibold", 14))
+        self.lres_entry.place(x=565, y=418, width=150)
+       
+        #Additional Label
+        self.adl_label = Label(self.window, text="Estos valores se tomarán para\n ambas señales. Si no escribe\n ninguno, se tomarán por defecto. ", bg="white", fg="#595958",font=("yu gothic ui", 9, "bold"))
+        self.adl_label.place(x=540, y=480)
+       
         #BUTTON
         self.submit = ImageTk.PhotoImage\
             (file='img\calculatebtn.png')
         self.submit_button = Button(self.window,image=self.submit, relief = "flat", borderwidth=0, background="#FFE65B",activebackground="#FFE65B", cursor="hand2", command=self.calculate)
-        self.submit_button.place(x=477,y=608)
+        self.submit_button.place(x=465,y=608)
 
     def heading_color(self):
         fg = random.choice(self.color)
